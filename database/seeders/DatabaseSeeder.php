6ab02ec5ac1274ff93594630eda67bfc369158c1
Juniorlk <juniorlekeu@gmail.com>
2024-01-20 17:57:51 +0100

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,5 +21,71 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // profils seeders
+        DB::table('profils')->insert([
+            'name' => 'President',
+            'created_at'=> now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('profils')->insert([
+            'name' => 'Secretaire',
+            'created_at'=> now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('profils')->insert([
+            'name' => 'Tresorier',
+            'created_at'=> now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('profils')->insert([
+            'name' => 'Membre',
+            'created_at'=> now(),
+            'updated_at' => now(),
+        ]);
+
+
+
+        //users seeders
+        DB::table('users')->insert([
+            'name' => 'junior',
+            'surname'=>'lk',
+            'sex'=>'M',
+            'email' => 'junior@gmail.com',
+            'password' => 'junior',
+            'profil_id'=>1,
+            'created_at'=> now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'arnold',
+            'surname'=>'tfoa2',
+            'sex'=>'M',
+            'email' => 'arnold@gmail.com',
+            'password' => 'arnold',
+            'profil_id'=>2,
+            'created_at'=> now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'borel',
+            'surname'=>'borel',
+            'sex'=>'M',
+            'email' => 'borel@gmail.com',
+            'password' => 'borel',
+            'profil_id'=>3,
+            'created_at'=> now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'gladys',
+            'surname'=>'gladys',
+            'sex'=>'F',
+            'email' => 'gladys@gmail.com',
+            'password' => 'gladys',
+            'profil_id'=>4,
+            'created_at'=> now(),
+            'updated_at' => now(),
+        ]);
     }
 }
